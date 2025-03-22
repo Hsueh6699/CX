0a076b8779410a9b90ae2bb21e83424454c68b89
Hsueh6699 <chiang112318@gmail.com>
2025-03-22 14:52:00 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'site-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './images/main-image.png',
-  './images/icon-192x192.png',
-  './images/icon-512x512.png',
-  './images/favicon.ico'
+  '/',
+  '/index.html',
+  '/images/main-image.png',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(event) {
+  // 执行安装步骤
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('缓存已打开');
         return cache.addAll(urlsToCache);
       })
   );
@@ -22,10 +22,27 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
+        // 如果找到缓存匹配项，则返回缓存
         if (response) {
           return response;
         }
         return fetch(event.request);
-      })
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 }); 
